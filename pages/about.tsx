1ab8486905260512a8bc6ebf1312f0ccbd37160a
bcko <bcko@pop-os.localdomain>
2020-08-09 16:21:55 -0700

    update about

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,7 +7,10 @@ const SectionOne = styled.section`
   grid-template-columns: 1fr 1fr;
 `;
 const LeftColumn = styled.div`
+  width: 500px;
   padding-top: 20px;
+  padding-left: 100px;
+  padding-right: 100px;
   text-align: center;
 `;
 
@@ -16,6 +19,12 @@ const Title = styled.div`
   font-family: 'Quicksand', sans-serif;
   font-weight: bold;
   color: #000000;
+  padding-bottom: 40px;
+`;
+
+const Paragraph = styled.p`
+  font-size: 15px;
+  padding: 15px;
 `;
 
 const RightColumn = styled.div`
@@ -40,26 +49,26 @@ export default function Page() {
       <SectionOne>
         <LeftColumn>
           <Title>What is Self-Distancing?</Title>
-          <p>
+          <Paragraph>
             When bad things happen, it is important to understand your feelings
             and thoughts after the event. However, that attempt often backfires,
             leading people to feel worse by reliving a trauma.
-          </p>
-          <p>
+          </Paragraph>
+          <Paragraph>
             Self-distancing is a research-based method that is proven to be an
             adaptive form of self-reflection (Ayduk & Kross, 2011; Park et al.,
             2016).
-          </p>
-          <p>
+          </Paragraph>
+          <Paragraph>
             Research shows that when people practice self-distancing, they are
             more likely to take control of their emotions, have less
             inter-personal issues, and also more likely to engage in productive
             problem-solving.
-          </p>
-          <p>
+          </Paragraph>
+          <Paragraph>
             The good news is that it is easy to learn how to do this. Let's
             learn how to do so.
-          </p>
+          </Paragraph>
         </LeftColumn>
         <RightColumn>
 
